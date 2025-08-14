fc886e65ce770b1d411375159d34fd6dc9cbd760
Mollaei <fe.mollaei@gmail.com>
2025-08-14 20:51:48 +0330

    feat: add keyboard support to calculator

--- js/script.js
+++ js/script.js
@@ -5,30 +5,74 @@ window.onload = function () {
 
   let currentInput = "";
   let afterEqual = false;
+
+  //click on buttons
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function () {
       let value = this.innerHTML;
-      if (value === "=") {
-        history.innerHTML = currentInput + " =";
-        currentInput = eval(currentInput).toString();
-        afterEqual = true;
-      } else if (value === "C") {
-        currentInput = "";
-        display.innerHTML = "";
-        history.innerHTML = "";
-        afterEqual = false;
-      } else if (value === "←") {
-        currentInput = currentInput.slice(0, -1);
-        display.innerHTML = currentInput;
-      } else {
-        if (afterEqual) {
-          currentInput = "";
-          afterEqual = false;
-        }
-        currentInput += value;
-      }
-
-      display.innerHTML = currentInput;
+      calculator(value);
     };
+  }
+  //keyboard key presses
+  document.addEventListener("keydown", function (event) {
+    let value = null;
+    switch (event.key) {
+      case "Enter":
+        value = "=";
+        break;
+      case "Backspace":
+        value = "←";
+        break;
+      case "c":
+      case "C":
+        value = "C";
+        break;
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+      case ".":
+        value = event.key;
+        break;
+    }
+    if (value !== null) calculator(value);
+  });
+
+  // calculator function :
+  function calculator(value) {
+    if (value === "=") {
+      history.innerHTML = currentInput + " =";
+      // Remove the last character if it's an operator before evaluating :
+      let lastChar = currentInput[currentInput.length - 1];
+      if (isNaN(lastChar)) {
+        currentInput = currentInput.slice(0, -1);
+      }
+      currentInput = eval(currentInput).toString();
+      afterEqual = true;
+    } else if (value === "C") {
+      currentInput = "";
+      display.innerHTML = "";
+      history.innerHTML = "";
+      afterEqual = false;
+    } else if (value === "←") {
+      currentInput = currentInput.slice(0, -1);
+    } else {
+      if (afterEqual) {
+        currentInput = "";
+        afterEqual = false;
+      }
+      currentInput += value;
+    }
+    display.innerHTML = currentInput;
   }
 };
